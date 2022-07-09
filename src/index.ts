31ea5898fb00d1b8a1e9c8aff3a65c948bacc548
David Elentok <3david@gmail.com>
2022-07-09 16:12:39 +0300

    feat(*): add basic cuboid

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log("hello");
+import { primitives } from "@jscad/modeling";
+
+const { cuboid } = primitives;
+
+function main() {
+  return cuboid({ size: [20, 40, 80] });
+}
+
+export = { main };
